a1e15d590d3363268f0aca7a8c2c972fd71854f2
Mateus Rezende <70781361+mrezende1@users.noreply.github.com>
2022-11-07 15:34:56 -0300

    ✨ feat: Pagina de login

--- cypress/integration/3-ALURAPIC/api-alurapic.spec.js
+++ cypress/integration/3-ALURAPIC/api-alurapic.spec.js
@@ -31,7 +31,7 @@ describe('Buscar fotos e dados', ()=> {
            expect(res.body.id).to.be.equal(1) 
            expect(res.body).to.have.property('email')
            expect(res.body.email).to.be.equal("flavio@alurapic.com.br") 
-        }
+        } 
 
         )
     })
