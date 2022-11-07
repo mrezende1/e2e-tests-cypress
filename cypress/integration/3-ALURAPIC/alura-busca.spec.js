ee066626b6d0babe209fd206a81893efc16e3bec
Mateus Rezende <70781361+mrezende1@users.noreply.github.com>
2022-11-07 15:34:12 -0300

    ✨ feat: Pagina de login

--- cypress/integration/3-ALURAPIC/alura-busca.spec.js
+++ cypress/integration/3-ALURAPIC/alura-busca.spec.js
@@ -8,6 +8,6 @@ describe('alura busca cursos', () => {
         cy.get('#header-barraBusca-form-campoBusca').type('java');
         cy.get('.header-barraBusca-form-submit').click();
         cy.get('h4.busca-resultado-nome')
-          .should('contain', 'Formação Java e Orientação a Objetos');
+          .should('contain', 'Formação Java e Orientação a Objetos'); 
     })
 })
